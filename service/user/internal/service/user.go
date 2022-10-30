256580242ddc0b38c54bd0f3bcf83f185754ebda
YanMingyu <yanmingyu55@gmail.com>
2022-10-30 20:22:37 +0800

    v1030

--- service/user/internal/service/user.go
+++ service/user/internal/service/user.go
@@ -2,30 +2,43 @@ package service
 
 import (
 	"context"
-
-	pb "user/api/user/v1"
+	"github.com/go-kratos/kratos/v2/log"
+	v1 "user/api/user/v1"
+	"user/internal/biz"
 )
 
 type UserService struct {
-	pb.UnimplementedUserServer
+	v1.UnimplementedUserServer
+
+	uc  *biz.UserUsecase
+	log *log.Helper
 }
 
-func NewUserService() *UserService {
-	return &UserService{}
+// NewUserService new a greeter service.
+func NewUserService(uc *biz.UserUsecase, logger log.Logger) *UserService {
+	return &UserService{uc: uc, log: log.NewHelper(logger)}
 }
 
-func (s *UserService) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserReply, error) {
-	return &pb.CreateUserReply{}, nil
-}
-func (s *UserService) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserReply, error) {
-	return &pb.UpdateUserReply{}, nil
-}
-func (s *UserService) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserReply, error) {
-	return &pb.DeleteUserReply{}, nil
-}
-func (s *UserService) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserReply, error) {
-	return &pb.GetUserReply{}, nil
-}
-func (s *UserService) ListUser(ctx context.Context, req *pb.ListUserRequest) (*pb.ListUserReply, error) {
-	return &pb.ListUserReply{}, nil
+// CreateUser create a user
+func (u *UserService) CreateUser(ctx context.Context, req *v1.CreateUserInfo) (*v1.UserInfoResponse, error) {
+	user, err := u.uc.Create(ctx, &biz.User{
+		Mobile:   req.Mobile,
+		Password: req.Password,
+		NickName: req.NickName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	userInfoRsp := v1.UserInfoResponse{
+		Id:       user.ID,
+		Mobile:   user.Mobile,
+		Password: user.Password,
+		NickName: user.NickName,
+		Gender:   user.Gender,
+		Role:     int32(user.Role),
+		Birthday: user.Birthday,
+	}
+
+	return &userInfoRsp, nil
 }
